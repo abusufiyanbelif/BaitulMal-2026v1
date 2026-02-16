9a74ab4633faa435ce10ff64429a3bf10152441f
Abusufiyan Belief <abusufiyan.belif@gmail.com>
2026-02-16 14:03:29 +0000

    Check this build errors and fix it

--- src/lib/firebase-admin-sdk.ts
+++ src/lib/firebase-admin-sdk.ts
@@ -1,5 +1,4 @@
 
-'use server';
 import { initializeApp, getApps, App, cert } from 'firebase-admin/app';
 import { getAuth, Auth } from 'firebase-admin/auth';
 import { getFirestore, Firestore } from 'firebase-admin/firestore';
